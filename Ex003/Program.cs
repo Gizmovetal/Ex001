f6d73fe614b01ca70a87f44dd5a252f1dc034707
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-08 17:59:01 +0300

    Ex003 v2

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 7;
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+Console.WriteLine(numberA);
 int result = numberA*numberB;
 Console.WriteLine(numberA+numberB);
 Console.WriteLine(result);
